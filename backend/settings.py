1e8ec684c10dde9c427f9cdf8be1d4b9d9d0005a
SANDHYA KARKI <sandhyakarki@SandhyaMacBook-Air.local>
2026-04-18 20:25:39 +0545

    setup Gmail SMTP settings for real-time email delivery

--- backend/settings.py
+++ backend/settings.py
@@ -132,5 +132,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # ==================================================
 # EMAIL SETTINGS 
 # ==================================================
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = 'admin@smartcafe.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'sandhyakarki506@gmail.com'
+EMAIL_HOST_PASSWORD = 'vnblgdnhgutswnhi'  
+DEFAULT_FROM_EMAIL = 'SmartCafe <sandhyakarki506@gmail.com>'
